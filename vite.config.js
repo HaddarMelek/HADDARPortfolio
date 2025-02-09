7d32e56a327f5ad78645c972de6925b761aab611
Melek HADDAR <haddarmalek23@gmail.com>
2025-02-09 20:49:22 +0100

    wip

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/HADDARPortfolio/',
+  base: '/HADDARPortfolio',
   plugins: [react()],
   css: {
     preprocessorOptions: {
